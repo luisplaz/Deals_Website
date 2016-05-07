b55f2d10822f65a9cc5b4abcac9dcd88f6bb52fc
luisplaz <luisplaz@hotmail.com>
2016-05-07 14:38:07 -0400

    Add fake comments to posts

--- app.js
+++ app.js
@@ -1,11 +1,41 @@
-angular.module('Save_With_Ray', [])
+angular.module('Save_With_Ray', ['ui.router'])
+
+.config([
+	'$stateProvider',
+	'$urlRouterProvider',
+	function($stateProvider, $urlRouterProvider) {
+
+		$stateProvider
+		.state('home', {
+			url: '/home',
+			templateUrl: '/home.html',
+			controller: 'MainCtrl'
+		});
+
+		.state('posts', {
+			url: '/posts/{id}',
+			templateUrl: '/posts.html',
+			controller: 'PostsCtrl'
+		});
+
+		$urlRouterProvider.otherwise('home');
+	}])
 
 .factory('posts', [function(){
-    var o = {
-    posts: []
-  };
-  return o;
+	var o = {
+		posts: []
+	};
+	return o;
 }])
+
+.controller('PostsCtrl', [
+	'$scope',
+	'$stateParams',
+	'posts',
+	$scope.post = posts.posts[$stateParams.id];
+	function($scope, $stateParams, posts){
+
+	}]);
 
 .controller('MainCtrl', [
 	'$scope',
@@ -21,6 +51,10 @@ angular.module('Save_With_Ray', [])
 				title: $scope.title,
 				link: $scope.link,
 				upvotes: 0
+				comments: [
+				{author: 'Joe', body: 'Cool post!', upvotes: 0},
+				{author: 'Bob', body: 'Great idea but everything is wrong!', upvotes: 0}
+				]
 			});
 			$scope.title = '';
 			$scope.link = '';
