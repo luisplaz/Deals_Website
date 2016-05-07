bb16869218efb19df8a3eb66d68b52077b0e4c7e
luisplaz <luisplaz@hotmail.com>
2016-05-07 13:08:55 -0400

    Add factory to app.js

--- app.js
+++ app.js
@@ -1,7 +1,33 @@
 angular.module('Save_With_Ray', [])
 
+.factory('posts', [function(){
+    var o = {
+    posts: []
+  };
+  return o;
+}])
+
 .controller('MainCtrl', [
 	'$scope',
-	function($scope){
-		$scope.test = 'Hello world!';
+	'posts',
+	function($scope, posts){
+
+		$scope.posts = posts.posts;
+
+		$scope.addPost = function(){
+
+			if(!$scope.title || $scope.title === '') { return; }
+			$scope.posts.push({
+				title: $scope.title,
+				link: $scope.link,
+				upvotes: 0
+			});
+			$scope.title = '';
+			$scope.link = '';
+		};
+
+		$scope.incrementUpvotes = function(post) {
+			post.upvotes += 1;
+		};
 	}]);
+
