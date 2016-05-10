b1bbc56ef3024714cdc1d8059ef45f0c59a331af
luisplaz <luisplaz@hotmail.com>
2016-05-10 18:36:29 -0400

    Change module name in mainCtrl

--- save_with_ray/app/assets/javascripts/home/mainCtrl.js
+++ save_with_ray/app/assets/javascripts/home/mainCtrl.js
@@ -1,21 +1,10 @@
-angular.module('saveray')
+angular.module('SaveWithRay')
 .controller('MainCtrl', [
 	'$scope',
 	'posts',
 	function($scope, posts){
 
 		$scope.posts = posts.posts;
-
-		$scope.addPost = function(){
-
-			if(!$scope.title || $scope.title === '') { return; }
-			posts.create({
-				title: $scope.title,
-				link: $scope.link,
-			});
-			$scope.title = '';
-			$scope.link = '';
-		};
 
 		$scope.incrementUpvotes = function(post) {
 			post.upvotes += 1;
