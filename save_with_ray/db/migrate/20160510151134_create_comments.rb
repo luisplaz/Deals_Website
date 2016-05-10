001b5fb5f08ffb55c018a520a2029fca6030f64c
luisplaz <luisplaz@hotmail.com>
2016-05-10 11:26:29 -0400

    Add attributes to comments table

--- save_with_ray/db/migrate/20160510151134_create_comments.rb
+++ save_with_ray/db/migrate/20160510151134_create_comments.rb
@@ -1,7 +1,9 @@
 class CreateComments < ActiveRecord::Migration
   def change
     create_table :comments do |t|
-
+      t.string :body
+      t.integer :upvotes
+      t.integer :post_id
       t.timestamps null: false
     end
   end
