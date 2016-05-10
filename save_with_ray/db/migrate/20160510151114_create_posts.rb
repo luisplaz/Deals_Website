f155ffb947619fa0b697ba5f9b02773538818fd2
luisplaz <luisplaz@hotmail.com>
2016-05-10 18:34:52 -0400

    Update attributes to post migration

--- save_with_ray/db/migrate/20160510151114_create_posts.rb
+++ save_with_ray/db/migrate/20160510151114_create_posts.rb
@@ -1,11 +1,11 @@
 class CreatePosts < ActiveRecord::Migration
-  def change
-    create_table :posts do |t|
-      t.string   :title
-      t.string   :body
-      t.string   :link
-      t.integer  :upvotes
-      t.timestamps null: false
-    end
-  end
+	def change
+		create_table :posts do |t|
+			t.string     :title
+			t.string     :link
+			t.string     :body
+			t.integer    :upvotes
+			t.timestamps  null: false
+		end
+	end
 end
