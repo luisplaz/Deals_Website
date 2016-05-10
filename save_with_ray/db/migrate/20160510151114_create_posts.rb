9aaac3471dbece8c4112c4cca1e165b1a7fcd47e
luisplaz <luisplaz@hotmail.com>
2016-05-10 11:16:39 -0400

    Add associations to posts

--- save_with_ray/db/migrate/20160510151114_create_posts.rb
+++ save_with_ray/db/migrate/20160510151114_create_posts.rb
@@ -1,7 +1,8 @@
 class CreatePosts < ActiveRecord::Migration
   def change
     create_table :posts do |t|
-
+      t.string :name
+      t.string :body
       t.timestamps null: false
     end
   end
