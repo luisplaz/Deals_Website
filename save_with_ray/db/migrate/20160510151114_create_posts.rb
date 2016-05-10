d03594f952821476d10c684caddcfbbf8b86704d
luisplaz <luisplaz@hotmail.com>
2016-05-10 11:26:21 -0400

    Add attributes to post table

--- save_with_ray/db/migrate/20160510151114_create_posts.rb
+++ save_with_ray/db/migrate/20160510151114_create_posts.rb
@@ -1,8 +1,10 @@
 class CreatePosts < ActiveRecord::Migration
   def change
     create_table :posts do |t|
-      t.string :name
-      t.string :body
+      t.string   :name
+      t.string   :body
+      t.string   :link
+      t.integer  :upvotes
       t.timestamps null: false
     end
   end
